b19781fe84330c30e0b5cfbe5cbe61598392a068
siavashre <siavash.raisi@gmail.com>
2023-05-24 17:26:13 -0700

    Segmentation with R

---
+++ segmentation.R
@@ -0,0 +1,11 @@
+library('DNAcopy')
+args = commandArgs(trailingOnly=TRUE)
+data <- read.csv(args[1])
+genomedata <- data[[4]]
+chrom <-rep(1,length(genomedata))
+w <- data[[5]]
+maploc <- c(1:length(genomedata))
+ans <- segment(CNA(genomedata, chrom, maploc), weights=w)
+#ans <- segment(CNA(genomedata, chrom, maploc))
+write.table(ans[2], args[2], append = FALSE, sep = "\t", dec = ".",
+            row.names = TRUE, col.names = TRUE)
